540ba2f926c35aaaef684c761034c4346112d655
MadhushaPrasad <madushaprasad21@gmail.com>
2021-04-08 18:45:48 +0530

    make as functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+import AboutUs from './pages/AboutUsPage/AboutUs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return <AboutUs />;
 }
 
 export default App;
