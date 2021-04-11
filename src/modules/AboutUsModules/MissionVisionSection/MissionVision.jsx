1e0c5256148140a953192da292e0872077df8af1
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-04-11 21:51:04 +0530

    turning off eslint rules

--- src/modules/AboutUsModules/MissionVisionSection/MissionVision.jsx
+++ src/modules/AboutUsModules/MissionVisionSection/MissionVision.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-self-import */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import './MissionVision.css';
 import FlatVector from '../../../assets/flat-illustrations/boardpeople.png';
@@ -8,20 +5,22 @@ import FlatVector from '../../../assets/flat-illustrations/boardpeople.png';
 const MissionVision = () => (
   <section className="mainSection">
     <div className="container main">
-      <div className="row text-left align-items-center pt-5 pb-md-5 ">
-        <div className="col-4 col-md-5">
-          <img alt="image" className="img-fluid" src={FlatVector} />
-        </div>
+      <div className="sec-1">
+        <div className="row text-left align-items-center pt-5 pb-md-5">
+          <div className="col-4 col-md-5">
+            <img alt="image" className="img-fluid" src={FlatVector} />
+          </div>
 
-        <div className="col-12 col-md-5">
-          <h2>
-            <strong>Our Mission</strong>
-          </h2>
-          <p className="lead">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum at egestas rutrum commodo ultrices
-            vitae vel. Velit a scelerisque et pharetra, tincidunt pulvinar amet. Tristique morbi enim urna viverra massa
-            vehicula quam.
-          </p>
+          <div className="col-12 col-md-5">
+            <h2>
+              <strong>Our Mission</strong>
+            </h2>
+            <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum at egestas rutrum commodo
+              ultrices vitae vel. Velit a scelerisque et pharetra, tincidunt pulvinar amet. Tristique morbi enim urna
+              viverra massa vehicula quam.
+            </p>
+          </div>
         </div>
       </div>
 
