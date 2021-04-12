437f08d5558878e1145afdc6099879e035d56b19
SenuraJayadeva <senurajayadeva@gmail.com>
2021-04-12 10:32:54 +0530

    Add What We Do Section - Home Page

--- src/modules/HomeModules/WhatWeDoSection/WhatWeDoSection.jsx
+++ src/modules/HomeModules/WhatWeDoSection/WhatWeDoSection.jsx
@@ -10,22 +10,6 @@ const WhatWeDoSection = () => (
     <div className="container">
       <div className="row mt-5 mb-5">
         <div className="col-md-6 col-sm-12">
-          <img alt="imageFlatVector" className="img-fluid d-none d-md-block" src={FlatVector} />
-        </div>
-        <div className="col-md-6 col-sm-12">
-          <h2>
-            <strong>Lorem ipsum</strong>
-          </h2>
-          <p className="lead">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum at egestas rutrum commodo ultrices
-            vitae vel. Velit a scelerisque et pharetra, tincidunt pulvinar amet. Tristique morbi enim urna viverra massa
-            vehicula quam.
-          </p>
-        </div>
-      </div>
-
-      <div className="row mt-5 mb-5">
-        <div className="col-md-6 col-sm-12">
           <h2>
             <strong>Lorem ipsum</strong>
           </h2>
@@ -53,6 +37,22 @@ const WhatWeDoSection = () => (
             vitae vel. Velit a scelerisque et pharetra, tincidunt pulvinar amet. Tristique morbi enim urna viverra massa
             vehicula quam.
           </p>
+        </div>
+      </div>
+
+      <div className="row mt-5 mb-5">
+        <div className="col-md-6 col-sm-12">
+          <h2>
+            <strong>Lorem ipsum</strong>
+          </h2>
+          <p className="lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum at egestas rutrum commodo ultrices
+            vitae vel. Velit a scelerisque et pharetra, tincidunt pulvinar amet. Tristique morbi enim urna viverra massa
+            vehicula quam.
+          </p>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <img alt="imageFlatVector" className="img-fluid d-none d-md-block" src={FlatVector} />
         </div>
       </div>
     </div>
