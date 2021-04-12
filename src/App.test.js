9b54bf6225f97297bda2e935326cd476eaa743d4
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-04-12 10:34:11 +0530

    update test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+/* global describe, test, document */
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-// eslint-disable-next-line no-undef
-test('renders learn react link', () => {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  // eslint-disable-next-line no-undef
-  expect(linkElement).toBeInTheDocument();
+describe('App.js', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
 });
