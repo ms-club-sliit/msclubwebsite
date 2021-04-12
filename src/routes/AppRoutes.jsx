c8670b437cd73fee8e3d0818b3bbc5e65b53951f
SenuraJayadeva <senurajayadeva@gmail.com>
2021-04-12 09:59:36 +0530

    Completed What We Do Section -Home Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutUs from '../pages/AboutUsPage/AboutUs';
+import Home from '../pages/HomePage/Home';
 
 const AppRoutes = () => (
   <div>
     <Router>
       <Switch>
         <Route path="/about" component={AboutUs} exact />
+        <Route path="/" component={Home} exact />
       </Switch>
     </Router>
   </div>
