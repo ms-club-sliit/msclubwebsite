a1aca9af889d3d53c55d52ce0ba4a0a6e39c82a9
it18028010 <it18028010@my.sliit.lk>
2021-06-03 22:48:29 +0530

    date,month added into JSON files

--- src/modules/HomeModules/EventCardComponent/EventCardComponent.jsx
+++ src/modules/HomeModules/EventCardComponent/EventCardComponent.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './EventCardStyle.css';
 
-const EventCardComponent = ({ image, title, time }) => (
+const EventCardComponent = ({ image, title, time, month, date }) => (
   <div className="event-card card mb-3 mt-3">
     <div className="past-event-img">
       <img className="past-event-img" src={image} alt="Card cap" />
     </div>
     <div className="day-container">
-      <h3 className="day">27</h3>
-      <p className="month">Nov</p>
+      <h3 className="day">{date}</h3>
+      <p className="month">{month}</p>
       <div className="time-container">
         <h5 className="time">{time}</h5>
       </div>
@@ -27,6 +27,8 @@ EventCardComponent.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   time: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  month: PropTypes.string.isRequired,
 };
 
 export default EventCardComponent;
