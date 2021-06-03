f0472739d1381614655c582e9cc7e90dcc11e874
it18028010 <it18028010@my.sliit.lk>
2021-06-03 21:59:21 +0530

    events card restructured

--- src/modules/HomeModules/EventCardComponent/EventCardComponent.jsx
+++ src/modules/HomeModules/EventCardComponent/EventCardComponent.jsx
@@ -4,10 +4,21 @@ import './EventCardStyle.css';
 
 const EventCardComponent = ({ image, title, time }) => (
   <div className="event-card card mb-3 mt-3">
-    <img className="past-event-img" src={image} alt="Card cap" />
+    <div className="past-event-img">
+      <img className="past-event-img" src={image} alt="Card cap" />
+    </div>
+    <div className="day-container">
+      <h3 className="day">27</h3>
+      <p className="month">Nov</p>
+      <div className="time-container">
+        <h5 className="time">{time}</h5>
+      </div>
+    </div>
     <div className="card-body">
       <h5 className="card-title">{title}</h5>
-      <p className="card-text">{time}</p>
+      <p className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit Morbi elementum at egestas rutrum commodo.
+      </p>
     </div>
   </div>
 );
