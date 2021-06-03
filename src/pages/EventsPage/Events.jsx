97bb780a859776e330988eb7a64aab67e6abb1aa
gihanrcg <it17016230@my.sliit.lk>
2021-06-03 18:19:51 +0530

    top speakers added

--- src/pages/EventsPage/Events.jsx
+++ src/pages/EventsPage/Events.jsx
@@ -3,12 +3,14 @@ import PastEvents from '../../modules/EventsModule/pastEvents/PastEvents';
 import UpcomingEventComponent from '../../modules/EventsModule/UpcomingEventComponent/UpcomingEventComponent';
 import Footer from '../../component/common/footer/Footer';
 import FutureEvents from '../../modules/EventsModule/futureEvents/FutureEvents';
+import TopSpeakers from '../../modules/EventsModule/topSpeakers/TopSpeakers';
 
 const Events = () => (
   <div>
     <UpcomingEventComponent />
     <PastEvents />
     <FutureEvents />
+    <TopSpeakers />
     <Footer />
   </div>
 );
